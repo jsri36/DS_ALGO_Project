edffcb5123da4fb8c6706a352cf43345acb4f292
jsri36 <jsri36@gmail.com>
2023-03-12 11:54:20 -0400

    testng runner

--- src/test/java/runner/DSALGO_Runner_Test.java
+++ src/test/java/runner/DSALGO_Runner_Test.java
@@ -2,15 +2,20 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
 import org.testng.annotations.DataProvider;
 
 //@RunWith(Cucumber.class) //Junit execution
 	
 @CucumberOptions(
-		plugin = {"pretty", "html:target/jsri36.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}, //reporting purpose
+		plugin = {"pretty", "html:target/cucumber-report.html",   //Cucumber Html report
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",  //Allure report
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"  //Extent report
+				}, //(Extent report reference - http://ghchirp.online/1697/  & https://www.youtube.com/watch?v=IuEO9AUbwwc&ab_channel=JahmalRichard)
 		monochrome=false,  //console output
 		tags = " ", //tags from feature file
-		features = "src/test/resources/features",
+		features = {"src/test/resources/features"
+		},
 		glue= "stepdefinitions") //location of step definition files
 
 public class DSALGO_Runner_Test extends AbstractTestNGCucumberTests{
